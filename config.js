6fc914c01f4147a7c1c04532eb8bd96aac07e09d
MorganHuegel <mhue2910@gmail.com>
2018-07-11 21:39:23 -0400

    dummy commit so that I can see Heroku changes

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/noteful',
   TEST_MONGO_URI: process.env.TEST_MONGO_URI || 'mongodb://localhost/noteful-test'
-}; 
+};
